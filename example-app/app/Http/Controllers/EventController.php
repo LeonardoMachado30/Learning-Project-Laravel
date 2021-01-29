5686833518e8d473232bf41dbc3963eb78d49176
Flavio Leonardo Machado <60984765+LeonardoMachado30@users.noreply.github.com>
2021-01-29 07:08:54 -0300

    Dashboard
    Criando uma Dashboard e apresentando as informações do usuario.

--- example-app/app/Http/Controllers/EventController.php
+++ example-app/app/Http/Controllers/EventController.php
@@ -89,5 +89,13 @@ class EventController extends Controller
         return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
 
     }
+
+    public function dashboard() {
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
+    }
 }
 
--- example-app/resources/views/dashboard.blade.php
+++ example-app/resources/views/dashboard.blade.php
@@ -1,15 +1,21 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.main')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
+@section('title', 'Dashboard')
+
+@section('content')
+    
+    <div class="col-md-10 offset-md1 dashboard-title-container">
+        <h2>Meus eventos</h2>
     </div>
-</x-app-layout>
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
+        @if(count(events) > 0)
+
+        @else
+            <p>voce ainda nao tem eventos, <a href="/events/create">Criar Eventos</a></p>
+        @endif
+    </div>
+
+
+
+
+@endsection
